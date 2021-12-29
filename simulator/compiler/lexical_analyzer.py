696b4054e697fa889c6bad8c85f6ed30fd28e045
Diego Fernandez <diegofdezsrz12@gmail.com>
2021-12-29 19:53:09 +0100

    Añadido el paquete del compilador y creado el modulo del lexico

---
+++ simulator/compiler/lexical_analyzer.py
@@ -0,0 +1,17 @@
+import ply.lex as lex
+
+
+class LexicalAnalyzer:
+
+    def __init__(self):
+        pass
+
+    tokens = (
+        'NUMBER',
+        'PLUS',
+        'MINUS',
+        'MULTIPLY',
+        'DIV',
+        'LPAREN',
+        'RPAREN'
+    )
