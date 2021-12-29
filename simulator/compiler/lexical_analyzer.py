092f6f6b969cbda379b2f946ccaa9b6ded413f91
Diego Fernandez <diegofdezsrz12@gmail.com>
2021-12-29 22:19:11 +0100

    Creados los tokens y palabras reservadas

--- simulator/compiler/lexical_analyzer.py
+++ simulator/compiler/lexical_analyzer.py
@@ -6,12 +6,73 @@ class LexicalAnalyzer:
     def __init__(self):
         pass
 
-    tokens = (
-        'NUMBER',
-        'PLUS',
-        'MINUS',
-        'MULTIPLY',
-        'DIV',
-        'LPAREN',
-        'RPAREN'
-    )
+    # Lista de nombres de tokens
+    tokens = [
+        'NUMBER',   # numeros
+        'ID',       # ids
+
+        # --COMPUESTOS--
+        'COPLUS',   # +=
+        'INC',      # ++
+        'COMINUS',  # -=
+        'DEC',      # --
+        'COMULT',   # *=
+        'CODIV',    # /=
+        'COREM',    # %=
+        'COBITXOR', # ^=
+        'COBITOR',  # |=
+        'COBITAND', # &=
+
+        # --COMPARACION--
+        'NEQ',      # !=
+        'EQUALS',   # ==
+        'LTHAN',    # <
+        'LEQTHAN',  # <=
+        'GTHAN',    # >
+        'GEQTHAN',  # >=
+
+        # --BOOLEANOS--
+        'NOT',      # !
+        'AND',      # &&
+        'OR',       # ||
+
+        # --OPERADORES CON BITS--
+        'BITAND',   # &         Cuidado con posibles conflictos con el & de puntero
+        'BITLEFT',  # <<
+        'BITRIGHT', # >>
+        'BITXOR',   # ^
+        'BITOR',    # |
+        'BITNOT',   # ~
+
+        # --ARITMETICOS--
+        'ASSIGN',   # =
+        'PLUS',     # +
+        'MINUS',    # -
+        'MULT',     # *
+        'DIV',      # /
+        'REM',      # %
+
+        # --OTROS--
+        'LPAREN',   # (
+        'RPAREN',   # )
+        'LBRACK',   # [
+        'RBRACK',   # ]
+        'LBRACE',   # {
+        'RBRACE',   # }
+        'SCOLON',   # ;
+    ]
+
+    reserved = {
+        # --CONTROL --
+        'while' : 'WHILE',
+        'do' : 'DO',
+        'break' : 'BREAK',
+        'continue' : 'CONTINUE',
+        'else' : 'ELSE',
+        'for' : 'FOR',
+        'goto' : 'GOTO',
+        'if' : 'IF',
+        'return' : 'RETURN',
+        'switch' : 'SWITCH',
+        'case' : 'CASE'
+    }
