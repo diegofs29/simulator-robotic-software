f91415619722d42c1c45d249724cfd2d05c727eb
Diego Fernandez <diegofdezsrz12@gmail.com>
2022-01-07 14:17:04 +0100

    Añadido listener para realizar los tests. Creada la clase de tests

--- simulator/compiler/__init__.py
+++ simulator/compiler/__init__.py
@@ -2,3 +2,4 @@ from . import ArduinoListener
 from . import ArduinoLexer
 from . import ArduinoParser
 from . import ArduinoListenerImpl
+from . import ArduinoListenerTests
--- simulator/tests/test_grammar.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from simulator.compiler.ArduinoListenerImpl import ArduinoListenerImpl
-
-class TestBase(unittest.TestCase):
-
-    def setUp(self):
-        a = ArduinoListenerImpl()
-        a.print_exit()
