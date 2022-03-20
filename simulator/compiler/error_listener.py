b1e093c5a5daddef122d38655a835baa141c3b3c
Diego Fernandez <diegofdezsrz12@gmail.com>
2022-03-20 23:11:46 +0100

    Tests de errores sintacticos, setup y loop

--- simulator/compiler/error_listener.py
+++ simulator/compiler/error_listener.py
@@ -1,26 +1,33 @@
 from distutils.log import error
 from antlr4.error.ErrorListener import ErrorListener
 from antlr4.error.Errors import *
+
+from simulator.compiler.ArduinoParser import ArduinoParser
 from ..console.console import Error
 
 
 class CompilerErrorListener(ErrorListener):
 
-    def __init__(self):
+    def __init__(self, log_errors=False):
         super().__init__()
         self.errors = []
+        self.log_errors = log_errors
 
     def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
-        error_type, message = self.__handle_error(e)
+        expected = self.__get_expected(recognizer)
+        error_type, message = self.__handle_error(e, expected, offendingSymbol)
         error = Error(error_type, line, column, message)
-        print(error.to_string())
+        if self.log_errors:
+            print(error.to_string())
         self.errors.append(error)
 
-    def __handle_error(self, e):
+    def __handle_error(self, e, expected, offending):
         error_type = message = ""
         if e == None:
             error_type = "Sintaxis"
             message = "Falta(n) caracter(es)"
+            if expected != None and expected[0] != '{':
+                message += ": {}".format(expected)
         elif type(e) == LexerNoViableAltException:
             error_type = "Léxico"
             message = "Caracter(es) inválido(s)"
@@ -28,8 +35,12 @@ class CompilerErrorListener(ErrorListener):
             error_type = "Sintaxis"
             message = "Código no válido"
         elif type(e) == InputMismatchException:
+            offending_text = offending.text.rstrip()
             error_type = "Sintaxis"
-            message = "El valor introducido en esta posición no es válido"
+            message = "El valor introducido en esta posición no es válido: {}".format(
+                offending_text)
+        # It would be strange for the following ones to appear, so they're not tested
+        # Although they're not discarded
         elif type(e) == FailedPredicateException:
             error_type = "Predicado"
             message = "Fallo al analizar predicado"
@@ -40,3 +51,11 @@ class CompilerErrorListener(ErrorListener):
             error_type = "?"
             message = "Error desconocido"
         return error_type, message
+
+    def __get_expected(self, recognizer):
+        try:
+            expected = recognizer.getExpectedTokens().toString(
+                recognizer.literalNames, recognizer.symbolicNames)
+            return expected.rstrip()
+        except AttributeError:
+            return None
--- tests/test_errors.py
+++ tests/test_errors.py
@@ -8,12 +8,12 @@ from simulator.compiler.error_listener import CompilerErrorListener
 from simulator.compiler.semantical_errors import SemanticAnalyzer
 
 
-class TestBaseAST(unittest.TestCase):
+class TestBaseErrors(unittest.TestCase):
 
     def setUp(self):
         input = FileStream(fileName=self.file, encoding="utf-8")
         lexer = ArduinoLexer(input)
-        error_listener = CompilerErrorListener()
+        error_listener = CompilerErrorListener(True)
         lexer.removeErrorListeners()
         lexer.addErrorListener(error_listener)
         stream = CommonTokenStream(lexer)
@@ -24,7 +24,7 @@ class TestBaseAST(unittest.TestCase):
         semantic = SemanticAnalyzer()
         tree = parser.program()
         self.syntax_errors = error_listener.errors
-        if len(self.syntax_errors)<1:
+        if len(self.syntax_errors) < 1:
             self.ast = visitor.visit(tree)
             semantic.visit_program(self.ast, None)
         self.semantic_errors = semantic.errors
@@ -33,9 +33,36 @@ class TestBaseAST(unittest.TestCase):
         return super().tearDown()
 
 
-class TestOtherErrors(TestBaseAST):
+class TestSyntaxErrors(TestBaseErrors):
 
     file = "tests/error-tests/lex-syn.txt"
 
-    def test_syntax_errors(self):
-        self.assertEqual(len(self.syntax_errors), 3)
+    def test_number_of_errors(self):
+        self.assertEqual(len(self.syntax_errors), 5)
+
+    def test_type_of_errors(self):
+        self.assertEqual(self.syntax_errors[0].error_type, "Sintaxis")
+        self.assertEqual(self.syntax_errors[1].error_type, "Sintaxis")
+        self.assertEqual(self.syntax_errors[2].error_type, "Léxico")
+        self.assertEqual(self.syntax_errors[3].error_type, "Sintaxis")
+        self.assertEqual(self.syntax_errors[4].error_type, "Sintaxis")
+
+    def test_error_messages(self):
+        self.assertEqual(self.syntax_errors[0].message, 
+            "El valor introducido en esta posición no es válido: int")
+        self.assertEqual(self.syntax_errors[1].message, 
+            "Falta(n) caracter(es): ';'")
+        self.assertEqual(self.syntax_errors[2].message, 
+            "Caracter(es) inválido(s)")
+        self.assertEqual(self.syntax_errors[3].message, 
+            "El valor introducido en esta posición no es válido: \"panda;")
+        self.assertEqual(self.syntax_errors[4].message, 
+            "Código no válido")
+
+
+class TestSetupLoopErrors(TestBaseErrors):
+
+    file = "tests/error-tests/setup-loop.txt"
+
+    def test_number_of_errors(self):
+        self.assertEqual(len(self.semantic_errors), 2)
